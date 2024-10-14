50403f75d15ed06fd3191c2f3aa30987bd016a43
Solano Matheus Martins <solanomatheusmartins@gmail.com>
2024-10-14 02:07:04 -0300

    refactor: Update video routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,21 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\VideoController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/videos', [VideoController::class, 'index']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::prefix('videos')->controller(VideoController::class)->group(function () {
+        Route::post('/', 'store');
+        Route::get('/{id}', 'show');
+        Route::patch('/{id}', 'update');
+        Route::patch('/{id}/like', 'toggleLike');
+    });
+});
